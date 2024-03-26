5341ae36f3d7cf602b302046abc460c9fdc94c56
mihaiFilipoiu <mihai.filipoiu@gmail.com>
2024-03-26 18:55:57 +0200

    models created and migrated

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,30 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=64)
+
+class AuctionListing(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=255)
+    starting_bid = models.IntegerField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="auctions")
+
+class Bid(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    bid = models.IntegerField()
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids_made")
+    bid_time = models.DateTimeField(auto_now_add=True)
+    auction = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="bids")
+
+class Comment(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    comment = models.CharField(max_length=255)
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments_made")
+    comment_time = models.DateTimeField(auto_now_add=True)
+    auction = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="comments")
+
